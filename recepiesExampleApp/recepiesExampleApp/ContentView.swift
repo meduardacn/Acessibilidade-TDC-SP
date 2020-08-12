4505b4358712cfbb2f4ede684f513fa6ac3e6af6
meduardcn <m.eduardacn@gmail.com>
2020-08-12 15:58:42 -0300

    project created

---
+++ recepiesExampleApp/recepiesExampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recepiesExampleApp
+//
+//  Created by Maria Eduarda Casanova Nascimento on 12/08/20.
+//  Copyright © 2020 Maria Eduarda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
